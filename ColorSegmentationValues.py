01b9af92130d0cd4d94c2d53232c02f869f9c44d
Ubuntu <ubuntu@ip-172-31-11-136.us-east-2.compute.internal>
2019-05-01 23:19:51 +0000

    S

--- ColorSegmentationValues.py
+++ ColorSegmentationValues.py
@@ -2,20 +2,20 @@ import numpy as np
 import cv2
 
 cap=cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
 while True:
     ret,frame = cap.read()
-    cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
     
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     
-    # Range for upper range
+    # Range for upper range for red
     lower_red = np.array([170,120,70])
     upper_red = np.array([180,255,255])
     mask = cv2.inRange(hsv,lower_red,upper_red)
     
     result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow(result)
+    cv2.imshow('Res',result)
 
 # When everything done, release the capture
 cap.release()
