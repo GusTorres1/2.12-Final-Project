fec4c82d3bb215762c69c81cfb5bb68c756e3636
sqliu343 <sqliu@mit.edu>
2019-05-08 12:13:54 -0400

    Update odrive_node_pizza.py
    Updated logic for picking up pizza toppings. Hopefully it works?

--- odrive_node_pizza.py
+++ odrive_node_pizza.py
@@ -14,6 +14,18 @@ import Queue
 deltaKin = kin.deltaSolver()
 
 pi = 3.1415
+balRad = 1.25 # inches
+pizRad = 4.0 # inches
+tab = 70 # inches
+
+in2mm = 25.4
+mm2in = 1/in2mm
+in2m = in2mm/1000
+
+#TODO: Put these values in, fool
+zhome = 0
+ztable = 0
+zdrop = 0
 
 class ODriveNode(object):
     def __init__(self):
@@ -21,47 +33,56 @@ class ODriveNode(object):
         
         self.command_queue = Queue.Queue(maxsize=5)
         rospy.Subscriber('/pos', Float32MultiArray, self.cmd_callback)
+        #rospy.Subscriber('/what_task', Int8, self.task_callback)
         
         self.vac_pub = rospy.Publisher('/toggle_vac', Int8, queue_size = 1)
-        self.cam_pub = rospy.Publisher('/camera_move', Bool, queue_size = 1)
+        self.task_pub = rospy.Publisher('/task_complete', Bool, queue_size = 1)
         
         self.thtDes = [0, 0, 0]
         bot.connect_all()
         print('ready')
+        
+        self.task_comp = False
 
     def cmd_callback(self, msg):
-        '''
-        TODO: Find the closest one and move to it, dawg
-        We should figure out that logic.
-        '''
-        print('hello')
-        self.pos = msg.data
-        self.thtDes = deltaKin.solveIt(self.pos)
-        bot.trajMoveRad(self.thtDes, 2*pi/8, 2*pi/8)
+        #print('hello')
+        if self.task_comp: # Only get the position values if task is complete
+            self.pos = msg.data # Get the proper value from the dictionary
+            if self.pizzaTop == True:
+                self.pos.append(ztable)
+            elif self.topDrop == True:
+                self.pos.append(zdrop)
+            self.thtDes = deltaKin.solveIt(self.pos)
+    
+    '''   
+    def task_callback(self, msg):
+        self.task = msg.data
+        # 0 - pick up topping
+        # 1 - drop topping
+        # 2 - move pizza
+        # 3 - punch dough
+        # 4 - parm shake
+        if self.task == 0 or self.task == 1:
+            self.pizzaTop = True
+        elif self.task == 2:
+            self.movePizza = True
+        elif self.task == 3:
+            self.punchDough = True
+        elif self.task == 4:
+            self.parmShake = True
+    '''
         
     def ifClose(self, tht):
         if bot.rad2Count(tht[0]) - bot.axis0.controller.pos_setpoint <= 500:
             if bot.rad2Count(tht[1]) - bot.axis1.controller.pos_setpoint <= 500:
                 if bot.rad2Count(tht[2]) - bot.axis2.controller.pos_setpoint <= 500:
+                    print("we get there homie")
                     return True
         return False
         
     def main_loop(self):
         main_rate = rospy.Rate(1)
-        pizzaTop = True
-        punchDough = False
-        movePizza = False
-        parmShake = False
-        
         vacInt = 2 # no vacuum
-        moveBool = True # Send one value
-        balRad = 1.25 # inches
-        pizRad = 4.0 # inches
-        tab = 70 # inches
-        
-        in2mm = 25.4
-        mm2in = 1/in2mm
-        in2m = in2mm/1000
         
         while not rospy.is_shutdown():
             '''
@@ -75,11 +96,43 @@ class ODriveNode(object):
             rospy.sleep(0.1)
             self.vac_pub.publish(1)
             '''
+            self.pizzaTop = True
+            self.topDrop = False
+            self.movePizza = False
+            self.punchDough = False
+            self.parmShake = False
             
-            #try:
-                #main_rate.sleep()
-            #except rospy.ROSInterruptException:
-                #break
+            try:
+                main_rate.sleep()
+            except rospy.ROSInterruptException:
+                break
+            if self.pizzaTop:
+                for i in range(9):
+                    self.task_comp = False
+                    self.task_pub.publish(self.task_comp)
+                    bot.trajMoveRad(self.thtDes, 2*pi/8, 2*pi/8)                
+                    self.vac_pub.publish(vacInt)
+                    rospy.sleep(0.1)
+                    if self.ifClose(self.thtDes):
+                        self.vac_pub.publish(0)
+                        self.task_comp = True
+                        self.task_pub.publish(self.task_comp)
+                        self.topDrop = True
+                    if self.topDrop:
+                        self.task_comp = False
+                        self.task_pub.publish(self.task_comp)
+                        bot.trajMoveRad(self.thtDes, 2*pi/8, 2*pi/8) 
+                        rospy.sleep(0.1)
+                        if self.ifClose(self.thtDes):
+                            self.vac_pub.publish(1)
+                            rospy.sleep(0.1)
+                            self.vac_pub.publish(2)
+                            self.task_comp = True
+                            self.topDrop = False
+                self.movePizza = True
+            if self.movePizza:
+                pass # TODO: Write code
+                
             #if pizzaTop:
                 #self.vac_pub.publish(vacInt)
                 #sleep(0.25) # Let vacuum activate for a bit
